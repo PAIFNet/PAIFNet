53bcecaa2ca108d32224db3482a105a5ea7a0064
= <=>
2024-11-15 14:43:05 -0300

    Cookies Corretos 2.0

--- paifnet/paif/urls.py
+++ paifnet/paif/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', login, name='login'),
-    path('recuperar-senha/', recuperar, name='recuperar'),
+    #path('recuperar-senha/', recuperar, name='recuperar'),
     path('verificar-login/', verificar_login, name="verificar_login"),
     path('dashboard/', dashboard, name="dashboard")
 ]
